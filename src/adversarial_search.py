fe0a0519634a8b631da85ac5c1a0ba3487372cce
hugocharels <hugocharels@hotmail.com>
2023-10-10 18:57:42 +0000

    wip minimax

--- src/adversarial_search.py
+++ src/adversarial_search.py
@@ -2,13 +2,42 @@ from lle import Action
 from mdp import MDP, S, A
 
 
-def minimax(mdp: MDP[A, S], state: S, max_depth: int) -> A:
-    ...
 
+def max_value_action(mdp: MDP[A, S], state: S, max_depth: int) -> (float, A):
+    best_action = Action.STAY
+    best_value = float('-inf')
+    for action in mdp.available_actions(state):
+        value = value_action(mdp, mdp.result(state, action), max_depth - 1)[0]
+        if value > best_value:
+            best_value = value
+            best_action = action
+    return best_value, best_action
+
+def min_value_action(mdp: MDP[A, S], state: S, max_depth: int) -> (float, A):
+    best_action = Action.STAY
+    best_value = float('inf')
+    for action in mdp.available_actions(state):
+        value = value_action(mdp, mdp.result(state, action), max_depth - 1)[0]
+        if value < best_value:
+            best_value = value
+            best_action = action
+    return best_value, best_action
+
+def value_action(mdp: MDP[A, S], state: S, max_depth: int) -> (float, A):
+    if mdp.is_final(state) or max_depth == 0:
+        return mdp.reward(state), Action.STAY
+    if state.current_agent == 0:
+        return max_value_action(mdp, state, max_depth)
+    else:
+        return min_value_action(mdp, state, max_depth)
+
+def minimax(mdp: MDP[A, S], state: S, max_depth: int) -> A:
+    """Returns the best action for the current agent to take in the given state, according to the minimax algorithm."""
+    return value_action(mdp, state, max_depth)[1]
 
 def alpha_beta(mdp: MDP[A, S], state: S, max_depth: int) -> A:
     ...
 
 
-def expectimax(mdp: MDP[A, S], state: S, max_depth: int) -> Action:
+def expectimax(mdp: MDP[A, S], state: S, max_depth: int) -> A:
     ...
