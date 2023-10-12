1ee4c1011c4c6332bd6ee9b447f10cc6979562ad
hugocharels <hugocharels@hotmail.com>
2023-10-12 18:57:47 +0000

    alpha_beta pruning

--- src/adversarial_search.py
+++ src/adversarial_search.py
@@ -35,8 +35,46 @@ def minimax(mdp: MDP[A, S], state: S, max_depth: int) -> A:
     if state.current_agent != 0: raise ValueError("The current agent must be 0.")
     return value_action(mdp, state, max_depth)[1]
 
+
+def alpha_beta_max_value_action(mdp: MDP[A, S], state: S, max_depth: int, alpha: float, beta: float) -> (float, A):
+    best_action = Action.STAY
+    best_value = float('-inf')
+    for action in mdp.available_actions(state):
+        value = alpha_beta_value_action(mdp, mdp.transition(state, action), max_depth - 1, alpha, beta)[0]
+        if value > best_value:
+            best_value = value
+            best_action = action
+        if value >= beta:
+            return value, action
+        alpha = value if value > alpha else alpha
+    return best_value, best_action
+
+def alpha_beta_min_value_action(mdp: MDP[A, S], state: S, max_depth: int, alpha: float, beta: float) -> (float, A):
+    best_action = Action.STAY
+    best_value = float('inf')
+    for action in mdp.available_actions(state):
+        value = alpha_beta_value_action(mdp, mdp.transition(state, action), max_depth - 1, alpha, beta)[0]
+        if value < best_value:
+            best_value = value
+            best_action = action
+        if value <= alpha:
+            return value, action
+        beta = value if value < beta else beta
+    return best_value, best_action
+
+def alpha_beta_value_action(mdp: MDP[A, S], state: S, max_depth: int, alpha: float, beta: float) -> (float, A):
+    if mdp.is_final(state) or max_depth == 0:
+        return state.value, Action.STAY
+    if state.current_agent == 0:
+        return alpha_beta_max_value_action(mdp, state, max_depth, alpha, beta)
+    else:
+        return alpha_beta_min_value_action(mdp, state, max_depth, alpha, beta)
+
+
 def alpha_beta(mdp: MDP[A, S], state: S, max_depth: int) -> A:
-    ...
+    """Returns the best action for the current agent to take in the given state, according to the alpha-beta algorithm."""
+    if state.current_agent != 0: raise ValueError("The current agent must be 0.")
+    return alpha_beta_value_action(mdp, state, max_depth, float('-inf'), float('inf'))[1]
 
 
 def expectimax(mdp: MDP[A, S], state: S, max_depth: int) -> A:
