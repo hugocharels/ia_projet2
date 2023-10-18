8ebf60339e35fafa689d77626fe0a08b1752840f
hugocharels <hugocharels@hotmail.com>
2023-10-18 21:47:26 +0200

    yolo

--- src/main.py
+++ src/main.py
@@ -3,40 +3,53 @@ from lle import World, Action
 from world_mdp import WorldMDP, BetterValueFunction
 from adversarial_search import minimax, alpha_beta, expectimax
 import csv
+import cv2
 
 
 WORLDS = [
 World("""
-.  . . . G G S0
-.  . . @ @ @ G
-S2 . . X X X G
-.  . . . G G S1
+. . . . .
+. G . G .
+. . . . .
+. X . X .
+. S0 . S1 .
 """
 ),
-
+World("""
+S1 G S0 G . X
+. . . . . X
+"""
+),
 ]
+
+#WORLDS[0].reset()
+#WORLDS[0].step([Action.WEST, Action.EAST])
 
 
 DEPTHS = [*range(1, 11)]
 
-WMDPS = (WorldMDP, BetterValueFunction)
+WMDPS = (WorldMDP,BetterValueFunction)
 
 ALGOS = ((alpha_beta, "alpha_beta"),)
 
 
 def main():
     results = []
-    for i in range(len(WORLDS)):
+    for i in range(len(WORLDS)-1):
+        cv2.imwrite("world_{}.png".format(i), WORLDS[i].get_image())
         for depth in DEPTHS:
+            print(depth)
             for WMDP in WMDPS:
                 for algo, name in ALGOS:
                     world = WMDP(WORLDS[i])
-                    action = algo(world, world.reset(), depth)
+                    s0 = world.reset()
+
+                    action = algo(world, s0, depth)
                     n_states = world.n_expanded_states
                     results.append([i, depth, WMDP.__name__, name, action, n_states])
 
     # Écrivez les résultats dans un fichier CSV
-    with open('results.csv', 'w', newline='') as csvfile:
+    with open('results_newworld1.csv', 'w', newline='') as csvfile:
         fieldnames = ['World', 'Depth', 'WMDP', 'Algorithm', 'Algorithm Name', 'Expanded States']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
